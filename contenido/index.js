32a6c9c8fc6d0101a962cd5e7cacdf4d3900e7d7
Ani  Leon <107953534+anileon@users.noreply.github.com>
2024-04-30 11:56:41 -0300

    initial commit

---
+++ contenido/index.js
@@ -0,0 +1,19 @@
+window.addEventListener("scroll", () => {
+    console.log("Scrolling")
+    const headerIntro = document.querySelector("#header");
+    if (window.scrollY > 100) {
+        headerIntro.classList.add("scrolled");
+    } else {
+        headerIntro.classList.remove("scrolled");
+    }
+});
+
+const menuToggle = document.querySelector('#menu-toggle');
+const menu = document.querySelector('#menu');
+
+menuToggle.addEventListener('click', () => {
+    menu.classList.toggle('active');
+});
+
+
+     
